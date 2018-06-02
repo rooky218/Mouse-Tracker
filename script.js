58cbaacc22ceee02034a65a3f2ed6a8acb4e31b6
Ben Walker <ben.walker218@gmail.com>
2018-06-02 12:04:34 -0400

    Glowing Circle Function
    add glowing circle function which adjusts the magnitude of the glow based on distance from center

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //Mouse Tracker is a simple app that tracks the mouse location and generates output
 const WINDOW_WIDTH = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 const WINDOW_HEIGHT = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+const HALF_WIDTH = WINDOW_WIDTH/2;
+const HALF_HEIGHT = WINDOW_HEIGHT/2;
+var output1 = document.getElementsByClassName("screen-info")[0];
+var myCircle = document.getElementsByClassName("mySpot")[0];
 
 function moveCircle(e) {
   let circle = document.getElementsByClassName("mySpot")[0];
@@ -24,61 +28,75 @@ function moveCircle(e) {
   circle.style.left = mirroredX + "px";
   circle.style.top = mirroredY + "px";
 
-  //set size
-  let output2 = mouseXLocation + mouseYLocation;
-  let size = document.getElementsByClassName("size")[0];
-  size.innerHTML = output2;
-
   //set color
   let centerX = mouseXLocation - mirroredX;
   let centerY = mouseYLocation - mirroredY;
+
   //console.log("CenterX: " + centerX + " CenterY: " + centerY);
   if(centerX <= 45 &&
     centerX >= -15 &&
     centerY <= 45 &&
     centerY >= -15){
-    circle.style.backgroundColor = "red";
+    circle.style.backgroundColor = "#990000";
+    //circle.style.boxShadow = " 0px 0px 50px rgba(0, 0, 0, 1), 0px 0px 50px rgba(153,0,0, .7)";
+
   } else {
     circle.style.backgroundColor = "#bcc3f9";
   }
 }
 
-function m2oveCircle(xin, yin){
+
+function adjustGlow(e){
   let circle = document.getElementsByClassName("mySpot")[0];
-  let radius = 30; //width = 60px
-  //console.log("Location called", radius);
+  let mouseXLocation = e.clientX;
+  let mouseYLocation = e.clientY;
 
-  //account for the size of circle
-  xin -= radius;
-  yin -= radius;
+  let myRatio = HALF_WIDTH/10;
 
-  //mirror mouse location
-  let mirroredLocation = mirrorMouse(xin, yin, radius);
-  //console.log("mirror", mirroredLocation);
+  //Find which half of screen on X Axis
+  if(mouseXLocation < HALF_WIDTH){
+    //console.log("Left Half of Screen", mouseXLocation);
 
-  circle.style.left = mirroredLocation.x + "px";
-  circle.style.top = mirroredLocation.y + "px";
-}
+    //ratio assumes HALF_WIDTH == 100%,
+    //then assignes mouseXLocation a percent value between 1 and 100
+    let mathX = Math.floor(100 * mouseXLocation / HALF_WIDTH);
+    let mathY = Math.floor(100 * mouseYLocation / HALF_HEIGHT);
 
-function mirrorMouse(xin, yin){
-  let xLocation = WINDOW_WIDTH - xin;
-  let yLocation = WINDOW_HEIGHT - yin;
+    console.log("In Q1", mathX);
+    circle.style.boxShadow = "0px 0px "+ mathX + "px rgba(188, 195, 249, .9), 0px 0px "+ mathY + "px rgba(188, 195, 249, .9)";
+    //0px 0px 40px rgba(0,173,255, .9),
+  } else {
+    //console.log("right half of screen", mouseXLocation);
+    let mathX2 = Math.floor(100 * (mouseXLocation - HALF_WIDTH) / HALF_WIDTH);
+    let mathY2 = Math.floor(100 * (mouseYLocation - HALF_HEIGHT) / HALF_HEIGHT);
 
-  if (xLocation <= zin && yLocation <= zin){
-    console.log("COLOR CHANGED");
-    document.getElementsByClassName("mySpot")[0].style.backgroundColor = "red";
-  }
+    console.log("BEFORE " + mathX2 + " : " + mathY2);
 
-  return {
-    x:xLocation,
-    y:yLocation
+
+    //invert percentage --- turn 90% into 10%
+    mathX2 = (mathX2 * -1) + 100;
+    mathY2 = (mathY2 * -1) + 100;
+
+    console.log("AFTER " + mathX2 + " : " + mathY2);
+
+    circle.style.boxShadow = "0px 0px "+ mathX2 + "px rgba(188, 195, 249, .9), 0px 0px "+ mathY2 + "px rgba(188, 195, 249, .9)";
   }
 }
 
 //document.getElementsByClassName("coordinates")[0].innerHTML = "Hello Sexy";
 document.addEventListener("mousemove", function(e){
-  let circle = document.getElementsByClassName("mySpot")[0];
-  circle.style.width = "60px";
-  circle.style.height = "60px";
+  //height/width are set once mouse enters screen. Before then the circle is invisible
+  myCircle.style.width = "60px";
+  myCircle.style.height = "60px";
   moveCircle(e);
+  adjustGlow(e);
 }, false);
+
+let message = "Screen X: " +
+WINDOW_WIDTH + "<br/>Screen Y: " +
+WINDOW_HEIGHT + "<br/>Half X: " +
+HALF_WIDTH + "<br/>Half Y: " +
+HALF_HEIGHT +
+"<br/><br/>Half / 10: " + Math.floor(HALF_HEIGHT/10);
+
+output1.innerHTML = message;
